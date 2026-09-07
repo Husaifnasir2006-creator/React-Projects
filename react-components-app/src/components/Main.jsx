8159f21d49b6d77b64970f48cfc5f73fd21f4795
Muhammad Husaif Bin Nasir <husaifnasir2006@gmail.com>
2026-09-07 18:27:16 +0500

    Initial commit

---
+++ react-components-app/src/components/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h2>Welcome to My React Website</h2>
+      <p>This is the main content of my React application.</p>
+    </main>
+  );
+}
+
+export default Main;
